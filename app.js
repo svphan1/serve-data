6ac9bf54365601615d10930b461f711be9cd43db
svphan29 <36240410+svphan29@users.noreply.github.com>
2018-11-05 11:09:13 -0700

    fix route and error handling

--- app.js
+++ app.js
@@ -2,21 +2,22 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const cohort = require('../routes/cohort');
-const error = require('../routes/error')
-const port = process.env.PORT || 3001;
+const cohorts = require('./routes/cohorts');
+const port = process.env.PORT || 3500;
 
 
-app.use(cors())
-app.use(bodyParser.urlencoded({ extended: false}))
+app.use(cors());
+app.use(bodyParser.json());
 
 app.get('/', (req, res, next) => {
-  res.send({cohort});
+  res.send({cohorts});
 });
 
 app.get('/:id', (req, res, next) => {
-  res.send(cohort[req.params.index-1])
-}) ;
+  const id = req.params.id
+  let theCohort = cohorts.filter(cohort => cohort.id == id)[0]
+  return (!Number(id) || id > cohorts.length) ? res.json({ error: { status: 400, message: "Please enter a valid ID number" } }) : res.json({ cohort: theCohort })
+});
 
 app.use(notFound);
 app.use(errorHandler);
@@ -34,4 +35,3 @@ function errorHandler(err, req, res, next) {
 app.listen(port, () => 
   console.log(`Your port is on ${port}`)
 );
-
